779cd1f69bab8dcb86cacd67dc12a8f65ae87480
lefty <geza.balogh@trayport.com>
2026-02-07 16:13:40 +0100

    partial solution

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::io::{self, BufRead};
+
+
+/*
+ * Complete the 'countResponseTimeRegressions' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY responseTimes as parameter.
+ */
+
+fn countResponseTimeRegressions(responseTimes: &[i32]) -> i32 {
+	// Write your code here
+	//println!("{:?}", &responseTimes);
+
+	let mut divisor = 0;
+	let mut counter = 0;
+	let mut subtotal = 0;
+
+
+	for rt in responseTimes {
+		divisor += 1;
+		if counter == 0 {
+			subtotal = *rt;
+		}
+		else {
+			subtotal = subtotal + *rt;
+			if rt > &(subtotal / divisor) {
+				counter += 1;
+			}
+		}
+	}
+	counter
+}
+
+fn main() {
+	let stdin = io::stdin();
+	let mut stdin_iterator = stdin.lock().lines();
+
+	let responseTimes_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+	let mut responseTimes: Vec<i32> = Vec::with_capacity(responseTimes_count as usize);
+
+	for _ in 0..responseTimes_count {
+		let responseTimes_item = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+		responseTimes.push(responseTimes_item);
+	}
+
+	let result = countResponseTimeRegressions(&responseTimes);
+
+	println!("{}", result);
+}
